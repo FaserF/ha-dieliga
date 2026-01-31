97f8aca09a10227172ef8eb75e0df0a9d15cb250
Fabian Seitz <seitzf1@yahoo.de>
2026-01-31 23:05:42 +0100

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,6 @@
 """Fixtures for tests."""
 import pytest
 
-import sys
-import os
-
 @pytest.fixture(autouse=True)
 def auto_enable_custom_integrations(enable_custom_integrations):
     """Enable custom integrations."""
